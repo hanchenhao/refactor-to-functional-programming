3f6900fd30a3347fb4076a319d6f6008b77d83a8
hanchenhao <hanchenhao@vip.qq.com>
2023-02-15 23:41:03 +0800

    finish

--- src/main/java/com/github/hcsp/functional/RefactorToSupplier.java
+++ src/main/java/com/github/hcsp/functional/RefactorToSupplier.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class RefactorToSupplier {
     private static int randomInt() {
@@ -19,9 +22,15 @@ public class RefactorToSupplier {
     // 请尝试使用函数式接口Supplier对三个方法进行重构，消除冗余代码
     // 并尽量尝试使用lambda表达式和方法引用来传递参数
     public static List<Object> create(Supplier<Object> supplier) {
-        return null;
+        return create(supplier, false);
     }
 
+    private static List<Object> create(Supplier<Object> supplier, boolean isSupplyString) {
+        return Stream.iterate(0, x -> x + 1)
+                .limit(10)
+                .map(x -> isSupplyString ? supplier.get() + x.toString() : supplier.get())
+                .collect(toList());
+    }
     public static List<Object> createObjects() {
         List<Object> result = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
